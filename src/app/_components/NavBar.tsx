a1f63eab154ef1581e17313d3b2b6357cef45472
Robchike9290 <robchike9290@gmail.com>
2025-11-06 16:23:58 -0600

    Make all pages visible when logged in, only landing and login page when logged out

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -9,19 +9,24 @@ interface NavBarProps {
 export default function NavBar({ isLoggedIn }: NavBarProps) {
     return (
         <nav className="flex w-full rounded-lg w-full border-blue-300 border-4 p-2 bg-blue-100">
-            <Link className="flex-1 text-blue-600 font-bold m-1 w-1/4 hover:text-purple-700 text-center text-2xl" href="/">Landing Page</Link>
-            <Link className="flex-1 text-blue-600 font-bold m-1 w-1/4 hover:text-purple-700 text-center text-2xl" href="/book">Booking Page</Link>
-            <Link className="flex-1 text-blue-600 font-bold m-1 w-1/4 hover:text-purple-700 text-center text-2xl" href="/profile">Profile</Link>
             {
                 isLoggedIn ? (
-                    <Link className="flex-1 text-blue-600 font-bold m-1 w-1/4 hover:text-purple-700 text-center text-2xl" href="/login">Logout</Link>
+                    <>
+                        <Link className="flex-1 text-blue-600 font-bold m-1 w-1/4 hover:text-purple-700 text-center text-2xl" href="/login">Logout</Link>
+                        <Link className="flex-1 text-blue-600 font-bold m-1 w-1/4 hover:text-purple-700 text-center text-2xl" href="/">Landing Page</Link>
+                        <Link className="flex-1 text-blue-600 font-bold m-1 w-1/4 hover:text-purple-700 text-center text-2xl" href="/book">Booking Page</Link>
+                        <Link className="flex-1 text-blue-600 font-bold m-1 w-1/4 hover:text-purple-700 text-center text-2xl" href="/profile">Profile</Link>
+                    </>
                 ) : (
-                    <Link 
-                        className="flex-1 text-blue-600 font-bold m-1 w-1/4 hover:text-purple-700 text-center text-2xl" 
-                        href="/login"
-                    >
-                        Login
-                    </Link>
+                    <>
+                        <Link className="flex-1 text-blue-600 font-bold m-1 w-1/4 hover:text-purple-700 text-center text-2xl" href="/">Landing Page</Link>
+                        <Link 
+                            className="flex-1 text-blue-600 font-bold m-1 w-1/4 hover:text-purple-700 text-center text-2xl" 
+                            href="/login"
+                        >
+                            Login
+                        </Link>
+                    </>
                 )
             }
         </nav>
