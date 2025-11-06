98cd908876c84ce8b88a44bb088ef6fe87f0544d
Robchike9290 <robchike9290@gmail.com>
2025-11-06 15:31:17 -0600

    Add some basic state and conditional rendering with notes for future behavior.

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link'
 export default function NavBar() {
     return (
         <nav className="flex w-full rounded-lg w-full border-blue-300 border-4 p-2 bg-blue-100">
-            <Link className="flex-1 text-blue-600 font-bold mt-2 w-1/4 hover:text-purple-700 text-center" href="/">Landing Page</Link>
-            <Link className="flex-1 text-blue-600 font-bold mt-2 w-1/4 hover:text-purple-700 text-center" href="/book">Booking Page</Link>
-            <Link className="flex-1 text-blue-600 font-bold mt-2 w-1/4 hover:text-purple-700 text-center" href="/profile">Profile</Link>
+            <Link className="flex-1 text-blue-600 font-bold m-1 w-1/4 hover:text-purple-700 text-center text-2xl" href="/">Landing Page</Link>
+            <Link className="flex-1 text-blue-600 font-bold m-1 w-1/4 hover:text-purple-700 text-center text-2xl" href="/book">Booking Page</Link>
+            <Link className="flex-1 text-blue-600 font-bold m-1 w-1/4 hover:text-purple-700 text-center text-2xl" href="/profile">Profile</Link>
         {/* TODO: Make conditional to only redirect when logging in, otherwise logging out and remaining on the landing page */}
-            <Link className="flex-1 text-blue-600 font-bold mt-2 w-1/4 hover:text-purple-700 text-center" href="/login">Login/Logout</Link>
+            <Link className="flex-1 text-blue-600 font-bold m-1 w-1/4 hover:text-purple-700 text-center text-2xl" href="/login">Login/Logout</Link>
         </nav>
     )
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import React, { useState } from 'react'
 
 export default function Login() {
     return (
@@ -10,25 +11,81 @@ export default function Login() {
 }
 
 function LoginForm() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value)
+    }
+
+    const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(event.target.value)
+    }
+
+    const handleSignup = () => {
+        // navigate to signup page
+    }
+
+    const handleSubmit = () => {
+        // check whether login/password combo is valid
+        // if so, navigate to profile page
+        // if not, navigate to signup screen with error message displayed.
+        console.log('submit handled!')
+    }
+
     return (
         <>
-            <h1 className="flex-1 text-blue-600 font-bold mt-2 hover:text-purple-700 text-center">Login</h1>
-            <form className="justify-self-center py-4 pb-8">
-                <div className="py-2">
-                    <label className="text-blue-700 text-center" htmlFor="Username">Username </label>
-                    <input type="text" />
-                </div>
-                <div className="py-2">
-                    <label className="text-blue-700 text-center" htmlFor="Username">Password </label>
-                    <input type="text" />
-                </div>
-                <div className="flex justify-end">
-                <button className="text-blue-600 font-bold hover:text-purple-700 rounded-md p-2 mt-2 bg-blue-300">Login</button>
-                </div>
-            </form>
-            <div>
-                <h3 className="flex justify-end gap-2 text-blue-600">Not a member?<a className="text-purple-700">Sign up here.</a></h3>
-            </div>
+        {
+            isLoggedIn ?
+            (
+                // TODO: Add loading state for logout process, have a logout function 
+                // automatically run and on completion or after 2 seconds, whichever is
+                // first, return to the landing page.
+                <h1>Logging out, please be patient...</h1>
+            ) : (
+                <>
+                    <h1 className="flex-1 text-blue-600 font-bold mt-2 hover:text-purple-700 text-center text-2xl">Login</h1>
+                    <form className="justify-self-center py-4 pb-8">
+                        <div className="py-2">
+                            <label className="text-blue-700 text-center text-lg" htmlFor="Username">Username </label>
+                            <input 
+                                className="text-blue-700"
+                                type="text" 
+                                onChange={handleUsernameChange}
+                                required
+                            />
+                        </div>
+                        <div className="py-2">
+                            <label className="text-blue-700 text-center text-lg" htmlFor="Username">Password </label>
+                            <input 
+                                className="text-blue-700"
+                                type="text" 
+                                onChange={handlePasswordChange} 
+                                required
+                            />
+                        </div>
+                        <div className="flex justify-end">
+                        <button 
+                            type="submit" 
+                            onClick={handleSubmit} 
+                            className="text-blue-600 font-bold hover:text-purple-700 rounded-md p-2 mt-2 bg-blue-300 shadow-xl"
+                        >
+                            Login
+                        </button>
+                        </div>
+                    </form>
+                    <div>
+                        <h3 className="flex justify-end gap-2 text-blue-600">
+                            Not a member?
+                            <a className="text-purple-700 hover:text-black" onClick={handleSignup}>
+                                Sign up here.
+                            </a>
+                        </h3>
+                    </div>
+                </>
+            )
+        }
         </>
     )
 }
