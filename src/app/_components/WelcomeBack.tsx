2d4c9ebd6d46235f45b26f8684d35e87bac298c9
Robchike9290 <robchike9290@gmail.com>
2025-11-20 19:04:10 -0600

    Attempt to get first name of user to show on welcome component, experiencing state issue

--- src/app/_components/WelcomeBack.tsx
+++ src/app/_components/WelcomeBack.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { usePlatformContext } from "@/_components/PlatformContext";
+import { profileData } from "@/data";
 
 export default function WeclomeBack() {
   let { currentUser } = usePlatformContext();
+  let [firstName, setFirstName] = useState("");
+
+  useEffect(() => {
+    profileData.forEach((profile) => {
+      console.log(profile.firstName);
+      if (profile.userName === currentUser) {
+        setFirstName("test");
+      }
+    });
+  });
 
   return (
     <>
       <h1 className="col-span-2 col-start-1 row-span-1 row-start-1 mt-[10vh] flex h-[20vh] justify-center text-3xl font-bold text-white">
-        Welcome Back, {currentUser}!
+        Welcome Back, {firstName}!
       </h1>
     </>
   );
